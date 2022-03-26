84a5bb237ccfca28f5e4ee709d555c18d598fb21
Juan Sáez García <contacto@juamber.com>
2022-03-26 16:59:59 +0100

    componentes app

--- 03-todoApp/src/app/app.module.ts
+++ 03-todoApp/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 03-todoApp/src/app/todos/todo.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
