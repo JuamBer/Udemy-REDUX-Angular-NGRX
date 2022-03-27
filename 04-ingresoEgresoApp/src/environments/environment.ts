076ea5d06163e9302078f08501713686fe7c653f
Juan Sáez García <contacto@juamber.com>
2022-03-27 18:28:45 +0200

    AngularFire fix

--- 04-ingresoEgresoApp/src/environments/environment.ts
+++ 04-ingresoEgresoApp/src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  firebaseConfig = {
+  firebaseConfig: {
     apiKey: "AIzaSyA0K932jV8nFfhVWkzKUehXUqZBtPW9dP8",
     authDomain: "ingreso-egreso-app-1d633.firebaseapp.com",
     projectId: "ingreso-egreso-app-1d633",
     storageBucket: "ingreso-egreso-app-1d633.appspot.com",
     messagingSenderId: "359178306335",
     appId: "1:359178306335:web:f9cb6b204f1b468e16e62a"
-  };
+  }
 };
 /*
  * For easier debugging in development mode, you can import the following file
