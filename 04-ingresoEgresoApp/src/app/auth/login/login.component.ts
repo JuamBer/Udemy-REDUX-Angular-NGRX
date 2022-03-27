7f88d9dc1a100a5aef6ccb4fae680f7d5870f842
Juan Sáez García <contacto@juamber.com>
2022-03-27 21:44:22 +0200

    SweetAlert2

--- 04-ingresoEgresoApp/src/app/auth/login/login.component.ts
+++ 04-ingresoEgresoApp/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -28,19 +29,37 @@ export class LoginComponent implements OnInit {
   loginUsuario() {
     console.log("loginUsuario()\n" + this.loginForm.value)
 
+    Swal.fire({
+      title: 'Validando Credenciales',
+      didOpen: () => {
+        Swal.showLoading()
+      }
+    })
+
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
       this.authService.loginUsuario(email, password)
         .then(
           (credenciales) => {
-            console.log(credenciales)
+            console.log(credenciales);
+            Swal.close();
             this.router.navigate(['/']);
           }
         ).catch(
           (error) => {
-            console.error(error)
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: error.message,
+            });
           }
         )
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Credenciales Inválidas',
+      })
     }
   }
 
